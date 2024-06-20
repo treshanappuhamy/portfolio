dfb2e9156a3e0cb07bafd1be5923e63f39128fbb
Treshan  Appuhamy <treshanappuhamy@gmail.com>
2024-06-21 00:51:58 +0800

    added loading.js and sub-loading.js instead of putting script in their relevant .html pages

---
+++ loading.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+    document.querySelector('.loading').style.display = 'block'; // Display main content
+    window.scrollTo(0, 0); // Scroll to top when the page is fully loaded
+    window.scrollTo(0, 0); // Scroll to top when the page is fully loaded
+    window.scrollTo(0, 0); // Scroll to top when the page is fully loaded
+    setTimeout(function() {
+        document.body.classList.remove('loading'); // Re-enable scrolling
+        document.querySelector('.container').style.display = 'block'; // Display main content
+        document.querySelector('.loading-page').style.display = 'none'; // Hide loading animation
+    }, 3000); // Adjust the time delay as needed
+});
