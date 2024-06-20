446c12713ac1be4ac9908f7b472c54588135baaa
Treshan  Appuhamy <treshanappuhamy@gmail.com>
2024-06-21 03:03:26 +0800

    added a loading spinner for the form

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,25 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzrF6lZYm5HVTjfY84wE18sK4rExkHQmhfatExAcf_YhfNcyiBsv-G3MLTeXOy_fkex/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzrF6lZYm5HVTjfY84wE18sK4rExkHQmhfatExAcf_YhfNcyiBsv-G3MLTeXOy_fkex/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+const sendingIndicator = document.querySelector('.sending-indicator');
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function (){
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
+    e.preventDefault();
 
+    // Show sending indicator only when submitting
+    sendingIndicator.style.display = 'flex';
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.textContent = "Message sent successfully";
+            setTimeout(function () {
+                msg.textContent = "";
+            }, 5000);
+            form.reset();
         })
         .catch(error => console.error('Error!', error.message))
-})
+        .finally(() => {
+            // Hide sending indicator after form submission is complete
+            sendingIndicator.style.display = 'none';
+        });
+});
