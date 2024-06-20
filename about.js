dd12905188ae493031a052623d3fc286425ece41
Treshan  Appuhamy <treshanappuhamy@gmail.com>
2024-06-21 00:36:06 +0800

    added a about.js instead of keeping the script in the index.html

---
+++ about.js
@@ -0,0 +1,13 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(event, tabname){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
